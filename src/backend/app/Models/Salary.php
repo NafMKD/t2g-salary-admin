4289a349b2a9af6c808fd919d332b667fcd8c4f0
NafMKD <guchemenberu32@gmail.com>
2025-08-19 00:13:01 +0300

    model done

--- src/backend/app/Models/Salary.php
+++ src/backend/app/Models/Salary.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Salary extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    /**
+     * Fillable attributes for mass assignment.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'salary_local',
+        'salary_euros',
+        'commission',
+    ];
+
+    /**
+     * Cast attributes.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'salary_local' => 'decimal:2',
+        'salary_euros' => 'decimal:2',
+        'commission' => 'decimal:2',
+    ];
 }
