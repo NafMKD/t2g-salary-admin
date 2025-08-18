7f4ff25bbe1b31fb0a51a2e96a61ab1e05395ea9
NafMKD <guchemenberu32@gmail.com>
2025-08-19 00:06:26 +0300

    migration done

---
+++ src/backend/database/migrations/2025_08_18_210526_create_salaries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
+            $table->decimal('salary_local', 10, 2)->default(0);
+            $table->decimal('salary_euros', 10, 2)->nullable();
+            $table->decimal('commission', 10, 2)->default(500.00);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salaries');
+    }
+};
